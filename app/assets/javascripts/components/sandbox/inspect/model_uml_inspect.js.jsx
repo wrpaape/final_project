268fbaa1583125716fcb930c6524fa5ed48e41c4
Reid Paape <wrpaape@gmail.com>
2015-07-10 21:27:34 -0500

    model UMLs pop up in inspector

--- app/assets/javascripts/components/sandbox/inspect/model_uml_inspect.js.jsx
+++ app/assets/javascripts/components/sandbox/inspect/model_uml_inspect.js.jsx
@@ -1,6 +1,34 @@
-var ModelUmlInspect = React.createClass({
+/* globals React */
+'use strict';
 
+var ModelUmlInspect = React.createClass({
+  getInitialState: function () {
+    return {
+      show: false
+    };
+  },
   render: function() {
-    return <div />;
+    var show = this.state.show;
+    var table = this.props.grandparent;
+    var currentModel = table.state.currentModel;
+    var umlFilePath = 'assets/' + currentModel + '_uml.png';
+
+    if (show) {
+      return(
+        <div className='row center'>
+          <div onClick={ this.clicked.bind(this, show) } className='btn btn-default show-hide'>UML</div>
+          <img src={ umlFilePath } className='image-uml'/>
+        </div>
+      );
+    } else {
+      return(
+        <div className='row center'>
+          <div onClick={ this.clicked.bind(this, show) } className='btn btn-primary show-hide'>UML</div>
+        </div>
+      );
+    };
+  },
+  clicked: function(show) {
+    this.setState({ show: !show })
   }
 });
