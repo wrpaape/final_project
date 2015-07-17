1dc48eca37b297cf96d9046d54d958e92d52b015
Reid Paape <wrpaape@gmail.com>
2015-07-16 21:20:50 -0500

    successfully***** seeded new environment

--- db/seeds.rb
+++ db/seeds.rb
@@ -311,181 +311,181 @@
 #   environment_id: baby_names_and_people.id)
 
 crops = {
-  :barley=> {
-    :price=> 5.62/40,
-    :yield=> 71.3*40
+  "barley"=> {
+    "price"=> 5.62/40,
+    "yield"=> 71.3*40
   },
-  :beans=> {
-    :price=> 35.8/100,
-    :yield=> 1753
+  "beans"=> {
+    "price"=> 35.8/100,
+    "yield"=> 1753.0
   },
-  :corn=> {
-    :price=> 4.11/56,
-    :yield=> 171*56
+  "corn"=> {
+    "price"=> 4.11/56,
+    "yield"=> 171*56
   },
-  :cotton=> {
-    :price=> 0.80,
-    :yield=> 838
+  "cotton"=> {
+    "price"=> 0.80,
+    "yield"=> 838.0
   },
-  :flaxseed=> {
-    :price=> 13.2/56,
-    :yield=> 21.1*56
+  "flaxseed"=> {
+    "price"=> 13.2/56,
+    "yield"=> 21.1*56
   },
-  :hay=> {
-    :price=> 178/2000,
-    :yield=> 2.45*2000
+  "hay"=> {
+    "price"=> 178.0/2000,
+    "yield"=> 2.45*2000
   },
-  :hops=> {
-    :price=> 1.83,
-    :yield=> 1868
+  "hops"=> {
+    "price"=> 1.83,
+    "yield"=> 1868.0
   },
-  :lentils=> {
-    :price=> 21.1/100,
-    :yield=> 1300
+  "lentils"=> {
+    "price"=> 21.1/100,
+    "yield"=> 1300.0
   },
-  :oats=> {
-    :price=> 3.54/60,
-    :yield=> 68.6*60
+  "oats"=> {
+    "price"=> 3.54/60,
+    "yield"=> 68.6*60
   },
-  :peanuts=> {
-    :price=> 0.23,
-    :yield=> 3932
+  "peanuts"=> {
+    "price"=> 0.23,
+    "yield"=> 3932.0
   },
-  :rice=> {
-    :price=> 15.6/100,
-    :yield=> 7572
+  "rice"=> {
+    "price"=> 15.6/100,
+    "yield"=> 7572.0
   },
-  :grain=> {
-    :price=> 7.41/100,
-    :yield=> 67.6*56
+  "grain"=> {
+    "price"=> 7.41/100,
+    "yield"=> 67.6*56
   },
-  :soybeans=> {
-    :price=> 12.5/60,
-    :yield=> 47.8*60
+  "soybeans"=> {
+    "price"=> 12.5/60,
+    "yield"=> 47.8*60
   },
-  :sunflower=> {
-    :price=> 21.7/100,
-    :yield=> 1469
+  "sunflower"=> {
+    "price"=> 21.7/100,
+    "yield"=> 1469.0
   },
-  :wheat=> {
-    :price=> 6.34/100,
-    :yield=> 44.3*100
+  "wheat"=> {
+    "price"=> 6.34/100,
+    "yield"=> 44.3*100
   },
-  :artichokes=> {
-    :price=> 50.4/100,
-    :yield=> 130*100
+  "artichokes"=> {
+    "price"=> 50.4/100,
+    "yield"=> 130.0*100
   },
-  :asparagus=> {
-    :price=> 111/100,
-    :yield=> 31*100
+  "asparagus"=> {
+    "price"=> 111.0/100,
+    "yield"=> 31.0*100
   },
-  :beets=> {
-    :price=> 65.8/100,
-    :yield=> 16.72*2000
+  "beets"=> {
+    "price"=> 65.8/100,
+    "yield"=> 16.72*2000
   },
-  :broccoli=> {
-    :price=> 35.1/100,
-    :yield=> 147*100
+  "broccoli"=> {
+    "price"=> 35.1/100,
+    "yield"=> 147.0*100
   },
-  ("brussel sprouts".to_sym)=> {
-    :price=> 36.5/100,
-    :yield=> 180*100
+  "brussel sprouts"=> {
+    "price"=> 36.5/100,
+    "yield"=> 180.0*100
   },
-  :cabbage=> {
-    :price=> 17.7/100,
-    :yield=> 357*100
+  "cabbage"=> {
+    "price"=> 17.7/100,
+    "yield"=> 357.0*100
   },
-  :carrots=> {
-    :price=> 33.1/100,
-    :yield=> 342*100
+  "carrots"=> {
+    "price"=> 33.1/100,
+    "yield"=> 342.0*100
   },
-  :cauliflower=> {
-    :price=> 46/100,
-    :yield=> 186*100
+  "cauliflower"=> {
+    "price"=> 46.0/100,
+    "yield"=> 186.0*100
   },
-  :celery=> {
-    :price=> 20/100,
-    :yield=> 636*100
+  "celery"=> {
+    "price"=> 20.0/100,
+    "yield"=> 636.0*100
   },
-  :mushrooms=> {
-    :price=> 7.79/6.55,
-    :yield=> 6.55*43560
+  "mushrooms"=> {
+    "price"=> 7.79/6.55,
+    "yield"=> 6.55*43560
   },
-  :cucumbers=> {
-    :price=> 26.6/100,
-    :yield=> 184*100
+  "cucumbers"=> {
+    "price"=> 26.6/100,
+    "yield"=> 184.0*100
   },
-  :eggplant=> {
-    :price=> 25.1/100,
-    :yield=> 291*100
+  "eggplant"=> {
+    "price"=> 25.1/100,
+    "yield"=> 291.0*100
   },
-  ("escarole & endive".to_sym)=> {
-    :price=> 29.5/100,
-    :yield=> 180*100
+  "escarole & endive"=> {
+    "price"=> 29.5/100,
+    "yield"=> 180.0*100
   },
-  :garlic=> {
-    :price=> 68.2/100,
-    :yield=> 163*100
+  "garlic"=> {
+    "price"=> 68.2/100,
+    "yield"=> 163.0*100
   },
-  ("collard greens".to_sym)=> {
-    :price=> 21.6/100,
-    :yield=> 119*100
+  "collard greens"=> {
+    "price"=> 21.6/100,
+    "yield"=> 119.0*100
   },
-  :lettuce=> {
-    :price=> 23.1/100,
-    :yield=> 366*100
+  "lettuce"=> {
+    "price"=> 23.1/100,
+    "yield"=> 366.0*100
   },
-  :cantaloupe=> {
-    :price=> 18.6/100,
-    :yield=> 236*100
+  "cantaloupe"=> {
+    "price"=> 18.6/100,
+    "yield"=> 236.0*100
   },
-  :okra=> {
-    :price=> 46.1/100,
-    :yield=> 60*100
+  "okra"=> {
+    "price"=> 46.1/100,
+    "yield"=> 60.0*100
   },
-  :onions=> {
-    :price=> 11.2/100,
-    :yield=> 523*100
+  "onions"=> {
+    "price"=> 11.2/100,
+    "yield"=> 523.0*100
   },
-  :peas=> {
-    :price=> 399/2000,
-    :yield=> 1.93*2000
+  "peas"=> {
+    "price"=> 399.0/2000,
+    "yield"=> 1.93*2000
   },
-  ("bell peppers".to_sym)=> {
-    :price=> 38.9/100,
-    :yield=> 330*100
+  "bell peppers"=> {
+    "price"=> 38.9/100,
+    "yield"=> 330.0*100
   },
-  :pumpkins=> {
-    :price=> 10.6/100,
-    :yield=> 266*100
+  "pumpkins"=> {
+    "price"=> 10.6/100,
+    "yield"=> 266.0*100
   },
-  :radishes=> {
-    :price=> 38.9/100,
-    :yield=> 92*100
+  "radishes"=> {
+    "price"=> 38.9/100,
+    "yield"=> 92.0*100
   },
-  :spinach=> {
-    :price=> 40.5/100,
-    :yield=> 158*100
+  "spinach"=> {
+    "price"=> 40.5/100,
+    "yield"=> 158.0*100
   },
-  :squash=> {
-    :price=> 38.1/100,
-    :yield=> 149*100
+  "squash"=> {
+    "price"=> 38.1/100,
+    "yield"=> 149.0*100
   },
-  ("sweet corn".to_sym)=> {
-    :price=> 26.6/100,
-    :yield=> 118*100
+  "sweet corn"=> {
+    "price"=> 26.6/100,
+    "yield"=> 118.0*100
   },
-  :tomatoes=> {
-    :price=> 42.5/100,
-    :yield=> 280*100
+  "tomatoes"=> {
+    "price"=> 42.5/100,
+    "yield"=> 280.0*100
   },
-  :apples=> {
-    :price=> 34400,
-    :yield=> 0.383
+  "apples"=> {
+    "price"=> 34400.0,
+    "yield"=> 0.383
   },
-  :strawberries=> {
-    :price=> 82.9/100,
-    :yield=> 505*100
+  "strawberries"=> {
+    "price"=> 82.9/100,
+    "yield"=> 505.0*100
   }
 }
 
@@ -504,15 +504,17 @@ crops.each do |crop, hash|
   Crop.create(name: crop, yield: hash["yield"])
   price_hash[crop] = hash["price"]
 end
+puts price_hash.inspect
 
 Client.all.each do |client|
   starting_revenue = client.revenue
   available_revenue = starting_revenue
   loop do
-    contract_total = rand(0.02..0.1)*starting_revenue
-    break unless (contract_total < available_revenue || clien.id == 13)
+    contract_total = rand(0.0025..0.01) * starting_revenue
+    puts contract_total
+    break unless (contract_total < available_revenue || client.id == 13)
     crop = Crop.all.sample
-    contract_price = rand(0.75..1.25)*crop.price
+    contract_price = rand(0.75..1.25) * price_hash[crop.name]
     contract_weight = contract_total / contract_price
     farmer = Farmer.all.sample
     Contract.create(weight: contract_weight,
@@ -529,7 +531,7 @@ Client.all.each do |client|
                upkeep: field_upkeep,
               farm_id: farm.id,
               crop_id: crop.id)
-    break if (available_revenue < 0 && clien.id == 13)
+    break if (available_revenue < 0 && client.id == 13)
   end
 end
 
