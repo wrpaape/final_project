5ca1bf467bb523b5204329329c1c31962c7eeccd
Reid Paape <wrpaape@gmail.com>
2015-08-01 22:33:27 -0500

    regex king documentation is now color coded

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,8 @@ description: "Sets attributes to be used when creating new records from a relati
 example: "users = User.where(name: 'Oscar')
 users.new.name # => 'Oscar'
 
-users = users.create_with(name: 'DHH')
-users.new.name # => 'DHH'
+users = users.create_with(name: 'Dhh')
+users.new.name # => 'Dhh'
 You can pass nil to create_with to reset attributes:
 
 users = users.create_with(nil)
@@ -1970,7 +1970,60 @@ end"
   }
 ]
 
-all_methods.each { |meth_params| ActRecMethod.create(meth_params) }
+def format_text(raw, ar_keywords)
+  split_lines = raw.split("\n")
+  formatted_lines = split_lines.map do |line|
+    comment_segs = line.split("# ")
+    formatted_segs = comment_segs.map.with_index do |comment_seg, i|
+      if i.odd?
+        comment_seg.prepend("▓@# ") << "▓"
+      else
+        ar_keywords.each do |ar_key|
+          comment_seg.gsub!(/(?<![A-Za-z_0-9])#{ar_key}(?![A-Za-z_0-9])/, "▓##{ar_key}▓")
+        end
+        sql_keywords = comment_seg.scan(/(?<![#])([A-H][A-Z]+|[J-Z][A-Z]+|[A-Z][A-C][A-Z]*|[A-Z][E-Z][A-Z]*|[A-Z][A-Z][A-Z]+|\(\*\)|\'\*\'| \* )/).flatten.uniq.compact
+        sql_keywords.each do |sql_key|
+          comment_seg.gsub!(sql_key, "▓?#{sql_key}▓")
+        end
+        rel_keywords = comment_seg.scan(/(?<![#?])([.:]*(address|friends|followers|posts|visible_posts)[:]*|relation(?=[\s\.]))/).flatten.uniq.compact
+        rel_keywords.each do |rel_key|
+          comment_seg.gsub!(rel_key, "▓&#{rel_key}▓")
+        end
+        gen_keywords = comment_seg.scan(/(?<![#?&])(?<=.)(each|map|with_index|size|midnight|now|com)[!]*/).flatten.uniq.compact
+        gen_keywords.each do |gen_key|
+          comment_seg.gsub!(gen_key, "▓%#{gen_key}▓")
+        end
+        attr_keywords = comment_seg.scan(/(?<![#?&%])([:][a-z_0-9]+|(?<![A-Za-z_0-9])[a-z_0-9]+:(?!$|:)|(?<=\.)[a-z_0-9]+)(?![a-z_0-9?!\(])/).flatten.uniq.compact
+        attr_keywords.each do |attr_key|
+          comment_seg.gsub!(attr_key, "▓*#{attr_key}▓")
+        end
+        val_keywords = comment_seg.scan(/(?<![#?&%*a-z_0-9])([0-9]+[0-9.]*|true|false|nil|(?<=\()[a-z_0-9]+(?=\))|(?<=#\{)[a-z_0-9]+(?=})|(?<=:▓ ['"])[A-Za-z_0-9]+(?=['"]))/).flatten.uniq.compact
+        val_keywords.each do |val_key|
+          comment_seg.gsub!(/(?<![`])#{val_key}/, "▓`#{val_key}▓")
+        end
+        model_keywords = comment_seg.scan(/(?<![#?&%*`])([A-Z][a-z]+(?=[.▓]))/).flatten.uniq.compact
+        model_keywords.each do |model_key|
+          comment_seg.gsub!(model_key, "▓~#{model_key}▓")
+        end
+      end
+      comment_seg
+    end
+    formatted_segs.join
+  end
+  formatted_lines.join("\n")
+end
+
+ar_keywords = all_methods.map { |ar_method| ar_method[:name] }
+all_methods.each.with_index do |ar_method, i|
+  ar_method.each do |key, val|
+    val ||= "nil"
+    if [:description, :example, :source].include?(key) && val.length > 48
+      all_methods[i][key] = format_text(val, ar_keywords)
+    end
+  end
+
+  ActRecMethod.create(ar_method)
+end
 
 env_descrip =
 """
